482728f8c7d5c9f173ecf44b998bc3a7a6400d1d
Jawn <john.karl.ford@gmail.com>
2025-03-04 09:33:31 -0600

    Initial commit

---
+++ CSC317_TheaterProject/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSC317_TheaterProject;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CSC317_TheaterProject/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CSC317_TheaterProject;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
